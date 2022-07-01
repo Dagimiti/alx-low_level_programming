b5fd8cfcef0b347e122d4f3b1da9358e50e4406e
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-01 08:03:11 -0700

    hi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int a;
-	char b;
 
-	for (a = 48; a < 58; a++)
+	for (a = 48; n < 58: a++)
 	{
 		putchar(a);
-	}
-	for (b = 'a'; b <= 'f'; b++)
-	{
-		putchar(b);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

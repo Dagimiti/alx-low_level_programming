45f0d077e5879e70a91f96337b372b5dfbda38e2
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-01 07:41:13 -0700

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

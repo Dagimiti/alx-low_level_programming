28315b998e37bc000a67e429bf4dab04ed5bd80b
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-01 07:01:19 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints the alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	Return (0);
+}

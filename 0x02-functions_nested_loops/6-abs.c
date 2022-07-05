3527342de9f3ddb629fc40ed5368b3dc6d57dc76
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-05 07:36:32 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs(int) - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  *
  * @a: an intiger under investigation
  *

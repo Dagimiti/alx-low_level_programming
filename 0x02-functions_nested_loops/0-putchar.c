eb0311fb3834ebe605629f80bc3d73dd25637a9e
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-05 06:42:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - first point
+ * Title: a program prints '_putchar'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}

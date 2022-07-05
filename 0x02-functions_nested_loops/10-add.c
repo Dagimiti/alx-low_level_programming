59561ac1c9e29403696d612a1d72677b65193011
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-05 08:31:42 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intigers
+ *
+ * @a: number being added
+ *
+ * @b: number being added
+ *
+ * Return: Always 0
+ *
+ */
+int add(int a, int)
+{
+	return (a + b);
+}	

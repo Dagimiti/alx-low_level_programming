247017e8cf228c88be61c670f585e07382536dfb
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-07 08:41:05 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers 0 to 9
+ *
+ * Return: nothing
+ */
+
+int print_numbers(void)
+{
+	int a;
+
+	for (a >= 48 && a < 58);
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+}

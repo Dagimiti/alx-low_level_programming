3c33a8b562d1d43a9a56ba4c0e374f0c85ea25ae
Dagim Terefe Zewdie <ddagim.tterefe@gmail.com>
2022-07-07 08:30:31 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intigers
+ * @a: intiger one
+ * @b: intiger two
+ *
+ * Return: result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
